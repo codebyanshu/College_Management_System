7f9a048286b8fb893e5585b215dbe116c2997a2e
hackeranshu007 <anshu290290@gmail.com>
2025-10-02 15:36:45 +0530

    colors added

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 
+let style = {
+  backgroundColor: "rgb(235, 170, 58)",
+  borderRadius: "20px",
+};
 
 function Login() {
   return (
     <>
+    <div style={{backgroundColor : "#FFDE63"}}>
       <Login_head />
-      <Login_body/>
+      <Login_body />
+      </div>
     </>
   );
 }
 
 function Login_head() {
   return (
-      <div className="headbody gap-20px">
-        <div className="cName"><h1>LKCTC</h1></div>
+    <div className="headbody" style={{ backgroundColor: "#FFCB61"}}>
+      <div className="cName">
+        <h1>LKCTC</h1>
+      </div>
       <div className="nav-bar">
         <ul className="nav nav-underline">
           <li className="nav-item">
@@ -37,47 +45,56 @@ function Login_head() {
         </ul>
       </div>
       <div id="menu-btn" className="fas fa-bars"></div>
-      </div>
+    </div>
   );
 }
 function Login_body() {
+
+
   return (
     <>
-      <div className="lbody container-lg">
-        <section className="home" id="home">
-          <div className="icon container">
-            <h3>LKTC</h3>
-          </div>
-        </section>
-        <section className="login_1 ">
-          <div className="login-form container shadow-lg p-3 mb-5 bg-white rounded boarder-2px">
-            <h2>Login</h2>
-            <form action="">
-              <input type="text" placeholder="Username" className="box my-2" />
-              <br />
+      <div className="icon">
+        <h3>LKTC</h3>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 d-flex justify-content-end">
+            <div
+              className="login-form shadow-lg p-3 mb-5 bg-body-warning border border-warning border-5"
+              style={style}
+            >
+              <h2>Login</h2>
+              <form action="">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className="box my-2"
+                />
+                <br />
 
-              <input
-                type="password"
-                placeholder="Password"
-                className="box my-2"
-              />
-              <br />
-              <input
-                type="submit"
-                value="Login"
-                className="btn btn-primary my-2"
-              />
-              <p>
-                Forget Password? <a href="#">Click Here</a>
-              </p>
-              <p>
-                Don't have an account? <a href="#">Create one</a>
-              </p>
-            </form>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="box my-2"
+                />
+                <br />
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-primary my-2"
+                />
+                <p>
+                  Forget Password? <a href="#">Click Here</a>
+                </p>
+                <p>
+                  Don't have an account? <a href="#">Create one</a>
+                </p>
+              </form>
+            </div>
           </div>
-        </section>
+        </div>
       </div>
     </>
   );
 }
-export default Login;
+      export default Login;
