7400601eef264ba41fbf1932fff73082d3510de3
hackeranshu007 <anshu290290@gmail.com>
2025-10-02 13:25:13 +0530

    login box design

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -12,27 +12,27 @@ function Login() {
 
 function Login_head() {
   return (
-      <div className="headbody">
+      <div className="headbody gap-20px">
         <div className="cName"><h1>LKCTC</h1></div>
       <div className="nav-bar">
-        <ul class="nav nav-underline">
-          <li class="nav-item">
-            <a class="nav-link active " aria-current="page" href="#">
+        <ul className="nav nav-underline">
+          <li className="nav-item">
+            <a className="nav-link active " aria-current="page" href="#">
               Home
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link link-secondary" href="#">
+          <li className="nav-item">
+            <a className="nav-link link-secondary" href="#">
               About
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link link-secondary" href="#">
+          <li className="nav-item">
+            <a className="nav-link link-secondary" href="#">
               Course
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link link-secondary">Contact Us</a>
+          <li className="nav-item">
+            <a className="nav-link link-secondary">Contact Us</a>
           </li>
         </ul>
       </div>
@@ -43,30 +43,39 @@ function Login_head() {
 function Login_body() {
   return (
     <>
-      <div className="lbody">
+      <div className="lbody container-lg">
         <section className="home" id="home">
-        <div className="icon">
-          <h3>LKTC</h3>
-        </div>
-      </section>
-      <section className="login_1">
-        <div className="login-form">
-          <h2>Login</h2>
-          <form action="">
-            <input type="text" placeholder="Username" className="box" />
-            <br />
+          <div className="icon container">
+            <h3>LKTC</h3>
+          </div>
+        </section>
+        <section className="login_1 ">
+          <div className="login-form container shadow-lg p-3 mb-5 bg-white rounded boarder-2px">
+            <h2>Login</h2>
+            <form action="">
+              <input type="text" placeholder="Username" className="box my-2" />
+              <br />
 
-            <input type="password" placeholder="Password" className="box" />
-            <input type="submit" value="Login" className="btn" />
-            <p>
-              Forget Password? <a href="#">Click Here</a>
-            </p>
-            <p>
-              Don't have an account? <a href="#">Create one</a>
-            </p>
-          </form>
-        </div>
-      </section>
+              <input
+                type="password"
+                placeholder="Password"
+                className="box my-2"
+              />
+              <br />
+              <input
+                type="submit"
+                value="Login"
+                className="btn btn-primary my-2"
+              />
+              <p>
+                Forget Password? <a href="#">Click Here</a>
+              </p>
+              <p>
+                Don't have an account? <a href="#">Create one</a>
+              </p>
+            </form>
+          </div>
+        </section>
       </div>
     </>
   );
